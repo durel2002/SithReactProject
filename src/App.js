7a0bf335d83d177c735b11a33ee6a910ed7e9402
durel2002 <durelhervane@gmail.com>
2023-09-05 19:59:28 +0100

    final app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import React, {useState} from "react";
 import './App.css';
 
 function App() {
+  const [toggle,setToggle] = useState(false);
+
+
+  const toggler = ()=>{
+    toggle?setToggle(false):setToggle(true);
+   
+
+  };
   return (
     <>
+    {toggle && <p> The Lord is God all the time </p>}
+    <br/>
+    <br/>
+
+    <button onClick={toggler}>{toggle?"HIDE":"SHOW"}</button>
     </>
   );
 }
